0a08bbc4971f288e5206b759e2c59124771df733
kgrant <keithgrant51@googlemail.com>
2017-01-22 00:46:23 +0000

    Added fighting robots

--- Scripts/GameState.gd
+++ Scripts/GameState.gd
@@ -3,7 +3,6 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-var timer = 0
 
 var red_goals = { 1: [
 Vector3(-170,-214,-85), 
@@ -17,24 +16,27 @@ Vector3(85, 128,170)]}
 var red_attempts = []
 var blue_attempts = []
 
-var game_round = 0
+var game_round = 1
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-
+	get_node("Table/Marker/AnimationPlayer").play("MarkerWave")
+	set_process_input(true)
 	set_fixed_process(true)
 	pass
 
+func _input(event):
+	if event.is_action_pressed("on_spacebar_hit"):
+		print("SpaceBar")
+		add_attempt()
+	pass
 
 func _fixed_process(delta):
-	timer += 1
-	#if(Input.is_action_pressed("on_spacebar_hit"):
+	#if(Input.is_action_pressed(("on_spacebar_hit")):
+		#print("SpaceBar")
+		#add_attempt()
 		
-	
-	if(timer > 100):
-		var game_node = get_node("./../")
-		#game_node.setScene("res://Scenes/GoodEndState.tscn")
 	pass
 
 func add_attempt():
@@ -42,17 +44,28 @@ func add_attempt():
 	var marker_node = get_node("./Table/Marker")
 	var marker_y = marker_node.get_pos().y
 	var attempt_x = marker_node.get_pos().x
-	if  (marker_y > 0):
+	if  (marker_y < 0):
+		print("Hit Red")
+		get_node("Redbot/RedbotBody/RedbotShoulder/RedbotForeArm/RedbotFist/AnimationPlayer").play("Punch")
 		calc_attempt_red(attempt_x)
 	else:
+		print("Hit Blue")
+		get_node("Bluebot1/BluebotBody/BluebotShoulder/BluebotForeArm/BluebotFist/AnimationPlayer").play("BluePunch")
 		calc_attempt_blue(attempt_x)
 	
 func calculate_winner():
+	print("Calculate Winner")
 	var red_score = get_score("Red")
+	print("Red Score:" + str(red_score))
 	var blue_score = get_score("Blue")
+	print("Blue Score:" + str(blue_score))
 	if(red_score < blue_score):
+		print("Red")
+		get_node("Bluebot1/BluebotBody/BluebotShoulder/BluebotForeArm/BluebotFist/AnimationPlayer").play("BlueHit")
 		return "Red"
 	else:
+		print("Blue")
+		get_node("Redbot/RedbotBody/RedbotShoulder/RedbotForeArm/RedbotFist/AnimationPlayer").play("RedHit")
 		return "Blue"
 	pass
 
@@ -60,7 +73,7 @@ func calc_attempt_blue(attempt_x):
 	var goal_array = blue_goals[game_round]
 	for hit in goal_array:
 		if(attempt_x >= hit.x && attempt_x < hit.z):
-			red_attempts.append(abs(hit.y - attempt_x))
+			blue_attempts.append(abs(hit.y - attempt_x))
 		
 	pass
 
