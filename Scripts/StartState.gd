0017db738c2916087e7282a83efb7591c0444f74
KTNelson <kierantnelson@gmail.com>
2017-01-22 02:29:06 +0000

    fonts fonts fonts

--- Scripts/StartState.gd
+++ Scripts/StartState.gd
@@ -7,7 +7,14 @@ extends Node2D
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
+	set_process_input(true)
+	
 	pass
+
+func _input(event):
+	if event.is_action_pressed("start_screen_start"):
+		var game_node = get_node("./../")
+		game_node.setScene("res://Scenes/GameState.tscn")
 
 
 func _on_Button_pressed():
